84727526c1d8cc14f5ee4b64780b2302f0bb63f6
Karthik D <karthik947@gmail.com>
2021-11-20 14:39:41 +0530

    refatored binance module

--- binance.js
+++ binance.js
@@ -15,47 +15,28 @@ module.exports = async (event) => {
       const url = baseURL + path + (queryString ? `?${queryString}` : '');
       const resp = await got(url);
       return { statusCode: 200, body: JSON.parse(resp.body) };
-    } else if (method === 'GET') {
-      //signed get request
-      params['timestamp'] = Date.now() - 1000;
-      params['recvWindow'] = 15000;
-      const queryString = Object.keys(params)
-        .map((d) => `${d}=${params[d]}`)
-        .join('&');
-      const { api, sec } = keys;
-      const signature = crypto
-        .createHmac('sha256', sec)
-        .update(queryString)
-        .digest('hex');
-      const url = `${baseURL}${path}?${queryString}&signature=${signature}`;
-      const options = {
-        headers: {
-          'X-MBX-APIKEY': api,
-        },
-      };
-      const resp = await got(url, options);
-      return { statusCode: 200, body: JSON.parse(resp.body) };
-    } else {
-      //signed post request
-      params['timestamp'] = Date.now() - 1000;
-      params['recvWindow'] = 15000;
-      const queryString = Object.keys(params)
-        .map((d) => `${d}=${params[d]}`)
-        .join('&');
-      const { api, sec } = keys;
-      const signature = crypto
-        .createHmac('sha256', sec)
-        .update(queryString)
-        .digest('hex');
-      const url = `${baseURL}${path}?${queryString}&signature=${signature}`;
-      const options = {
-        headers: {
-          'X-MBX-APIKEY': api,
-        },
-      };
-      const resp = await got.post(url, options);
-      return { statusCode: 200, body: JSON.parse(resp.body) };
     }
+
+    //Signed Requests
+    params['timestamp'] = Date.now() - 1000;
+    params['recvWindow'] = 15000;
+    const queryString = Object.keys(params)
+      .map((d) => `${d}=${params[d]}`)
+      .join('&');
+    const { api, sec } = keys;
+    const signature = crypto
+      .createHmac('sha256', sec)
+      .update(queryString)
+      .digest('hex');
+    const url = `${baseURL}${path}?${queryString}&signature=${signature}`;
+    const options = {
+      headers: {
+        'X-MBX-APIKEY': api,
+      },
+    };
+    const resp =
+      method === 'GET' ? await got(url, options) : await got.post(url, options);
+    return { statusCode: 200, body: JSON.parse(resp.body) };
   } catch (err) {
     throw JSON.stringify(err);
   }
